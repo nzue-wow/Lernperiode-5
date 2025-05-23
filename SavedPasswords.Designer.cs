711bcaee174d32ba5fbda87b5ad83a95496f0d85
Nena <N.zuehlke.inf24@stud.bbbaden.ch>
2025-05-23 11:01:48 +0200

    Add files via upload

--- SavedPasswords.Designer.cs
+++ SavedPasswords.Designer.cs
@@ -18,46 +18,72 @@
 
         private void InitializeComponent()
         {
-            this.listBoxPasswords = new System.Windows.Forms.ListBox();
-            this.buttonDelete = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            listBoxPasswords = new System.Windows.Forms.ListBox();
+            buttonDelete = new System.Windows.Forms.Button();
+            textBoxSuche = new System.Windows.Forms.TextBox();
+            buttonSuche = new System.Windows.Forms.Button();
+            SuspendLayout();
             // 
             // listBoxPasswords
             // 
-            this.listBoxPasswords.FormattingEnabled = true;
-            this.listBoxPasswords.ItemHeight = 25;
-            this.listBoxPasswords.Location = new System.Drawing.Point(20, 20);
-            this.listBoxPasswords.Name = "listBoxPasswords";
-            this.listBoxPasswords.Size = new System.Drawing.Size(1131, 579);
-            this.listBoxPasswords.TabIndex = 0;
+            listBoxPasswords.FormattingEnabled = true;
+            listBoxPasswords.ItemHeight = 32;
+            listBoxPasswords.Location = new System.Drawing.Point(22, 26);
+            listBoxPasswords.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            listBoxPasswords.Name = "listBoxPasswords";
+            listBoxPasswords.Size = new System.Drawing.Size(1225, 644);
+            listBoxPasswords.TabIndex = 0;
             // 
             // buttonDelete
             // 
-            this.buttonDelete.Location = new System.Drawing.Point(1044, 621);
-            this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(107, 46);
-            this.buttonDelete.TabIndex = 1;
-            this.buttonDelete.Text = "Delete";
-            this.buttonDelete.UseVisualStyleBackColor = true;
+            buttonDelete.Location = new System.Drawing.Point(1131, 795);
+            buttonDelete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            buttonDelete.Name = "buttonDelete";
+            buttonDelete.Size = new System.Drawing.Size(116, 59);
+            buttonDelete.TabIndex = 1;
+            buttonDelete.Text = "Delete";
+            buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSuche
+            // 
+            textBoxSuche.Location = new System.Drawing.Point(12, 692);
+            textBoxSuche.Name = "textBoxSuche";
+            textBoxSuche.Size = new System.Drawing.Size(331, 39);
+            textBoxSuche.TabIndex = 2;
+            // 
+            // buttonSuche
+            // 
+            buttonSuche.Location = new System.Drawing.Point(349, 692);
+            buttonSuche.Name = "buttonSuche";
+            buttonSuche.Size = new System.Drawing.Size(173, 42);
+            buttonSuche.TabIndex = 3;
+            buttonSuche.Text = "Suchen";
+            buttonSuche.UseVisualStyleBackColor = true;
+            buttonSuche.Click += ButtonSuche_Click;
             // 
             // SavedPasswords
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LavenderBlush;
-            this.ClientSize = new System.Drawing.Size(1174, 679);
-            this.Controls.Add(this.buttonDelete);
-            this.Controls.Add(this.listBoxPasswords);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "SavedPasswords";
-            this.Text = "Gespeicherte Passwörter";
-            this.Load += new System.EventHandler(this.SavedPasswords_Load);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.LavenderBlush;
+            ClientSize = new System.Drawing.Size(1272, 869);
+            Controls.Add(buttonSuche);
+            Controls.Add(textBoxSuche);
+            Controls.Add(buttonDelete);
+            Controls.Add(listBoxPasswords);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Name = "SavedPasswords";
+            Text = "Gespeicherte Passwörter";
+            Load += SavedPasswords_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox textBoxSuche;
+        private System.Windows.Forms.Button buttonSuche;
     }
 }
--- SavedPasswords.cs
+++ SavedPasswords.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace PasswortManager
 {
@@ -25,17 +27,27 @@ namespace PasswortManager
 
         private void LoadPasswords()
         {
-            if (listBoxPasswords == null)
-            {
-                MessageBox.Show("Fehler: listBoxPasswords existiert nicht!");
-                return;
-            }
+            listBoxPasswords.Items.Clear();
 
-            
+            string connectionString = "Data Source=\"C:\\Users\\nenaz\\source\\repos\\PasswortManager\\SQL_PasswortmanagerDB.db\"";
 
-            foreach (var entry in Form1.passwortListe)
+            using (var connection = new SqliteConnection(connectionString))
             {
-                listBoxPasswords.Items.Add($"{entry.Website}: {entry.Passwort}");
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT WebseitenName, Passwort FROM SavedPasswords";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string website = reader.GetString(0);
+                        string passwort = reader.GetString(1);
+
+                        listBoxPasswords.Items.Add($"{website}: {passwort}");
+                    }
+                }
             }
         }
 
@@ -51,6 +63,42 @@ namespace PasswortManager
             else
             {
                 MessageBox.Show("Bitte wählen Sie ein Passwort zum Löschen aus.");
+            }
+        }
+
+        private void ButtonSuche_Click(object sender, EventArgs e)
+        {
+            string suchbegriff = textBoxSuche.Text.Trim().ToLower();
+
+            listBoxPasswords.Items.Clear();
+
+            string connectionString = "Data Source=\"C:\\Users\\nenaz\\source\\repos\\PasswortManager\\SQL_PasswortmanagerDB.db\"";
+
+            using (var connection = new Microsoft.Data.Sqlite.SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT WebseitenName, Passwort FROM SavedPasswords WHERE LOWER(WebseitenName) LIKE @suchbegriff";
+                command.Parameters.AddWithValue("@suchbegriff", "%" + suchbegriff + "%");
+
+                using (var reader = command.ExecuteReader())
+                {
+                    bool gefunden = false;
+                    while (reader.Read())
+                    {
+                        string website = reader.GetString(0);
+                        string passwort = reader.GetString(1);
+
+                        listBoxPasswords.Items.Add($"{website}: {passwort}");
+                        gefunden = true;
+                    }
+
+                    if (!gefunden)
+                    {
+                        listBoxPasswords.Items.Add("Keine Einträge gefunden.");
+                    }
+                }
             }
         }
 
